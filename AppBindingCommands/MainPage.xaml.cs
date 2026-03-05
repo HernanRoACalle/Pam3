3fe7246616eae1ef5b2fb5ff696ea1f015752969
Rafael Martins Nogueira <rafamartnogueira@gmail.com>
2026-03-04 22:26:18 -0300

    aula inicial

---
+++ AppBindingCommands/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿namespace AppBindingCommands
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object? sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void lblInformacoess_Clicked(object sender, EventArgs e)
+        {
+            string informacoes = string.Empty;
+
+            if (Preferences.ContainsKey("AcaoInicial"))
+                informacoes += Preferences.Get("AcaoInicial", String.Empty);
+
+            if (Preferences.ContainsKey("AcaoStart"))
+                informacoes += Preferences.Get("AcaoStart", String.Empty);
+
+            if (Preferences.ContainsKey("AcaoSleep"))
+                informacoes += Preferences.Get("AcaoSleep", String.Empty);
+
+            if (Preferences.ContainsKey("AcaoResume"))
+                informacoes += Preferences.Get("AcaoResume", String.Empty);
+
+            lblInformacoes.Text = informacoes;
+        }
+    }
+}
